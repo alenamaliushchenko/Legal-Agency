4e13f6aaaf9e69488694462f8b64bfc53cad674b
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-04-07 15:42:00 +0300

    added footer

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 });
+
+
+// Ініціалізація Swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
